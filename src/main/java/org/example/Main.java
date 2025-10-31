c06281134abf9f03938b2e31608ad3d41f18d3b8
Ali Yücel <ali.yucel675@hotmail.com>
2025-11-01 02:58:46 +0300

    proje tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Player player = new Player("John", 100, Weapon.SWORD);
+        Point point = new Point(6, 5);
+
+        System.out.println("Player: " + player.healthRemaining());
+        System.out.println("Point: " + point.distance());
     }
 }
